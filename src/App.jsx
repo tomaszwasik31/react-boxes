ab7ef60322da0cd8bcda9c97160ac5cc08806ffe
Tomasz Wasik <tomaszwasik31@gmail.com>
2023-02-10 16:33:05 +0000

    base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import boxes from "./boxes";
+
+/**
+ * Challenge part 1:
+ * 1. Initialize state with the default value of the
+ *    array pulled in from boxes.js
+ * 2. Map over that state array and display each one
+ *    as an empty square (black border, transparent bg color)
+ *    (Don't worry about using the "on" property yet)
+ */
 
 function App() {
-  return (
-    <p>
-      Cleanedup React App
-    </p>
-  );
+  const [box, setBox] = React.useState(boxes);
+
+ const boxElements =  box.map((square) => <div className="box" key={square.id} />);
+  return <div className="box--wrapper">
+    {boxElements}
+  </div>
 }
 
 export default App;
