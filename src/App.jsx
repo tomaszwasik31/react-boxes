dd5b8885b9127f6b1250885e2278f90f755d9a6a
Tomasz Wasik <tomaszwasik31@gmail.com>
2023-02-13 12:11:27 +0000

    box toggle function

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,7 @@ import "./App.css";
 import Box from "./Box";
 import boxes from "./boxes";
 
-/**
- * Challenge part 2:
- * 1. Create a separate component called "Box" and
- *    replace the `div` above with our <Box /> components
- * 2. Pass the Box component a prop called `on` with the
- *    value of the same name from the `boxes` objects
- * 3. In the Box component, apply dynamic styles to determine
- *    the backgroundColor of the box. If it's `on`, set the
- *    backgroundColor to "#222222". If off, set it to "none"
- */
+   
 
 function App() {
   
