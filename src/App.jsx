61656a5290d8cf488d1fc2fe7d51610b9d9b9c1c
Tomasz Wasik <tomaszwasik31@gmail.com>
2023-02-13 16:53:34 +0000

    better practice solution update

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,43 @@ import "./App.css";
 import Box from "./Box";
 import boxes from "./boxes";
 
-   
+/**
+ * Challenge: use setSquares to update the
+ * correct square in the array.
+ *
+ * Make sure not to directly modify state!
+ *
+ * Hint: look back at the lesson on updating arrays
+ * in state if you need a reminder on how to do this
+ */
 
 function App() {
-  
   const [box, setBox] = React.useState(boxes);
 
+  // function toggle(id) {
+  //   setBox((prevBoxes) => {
+  //     return prevBoxes.map((prevBox) => {
+  //       if (prevBox.id === id) {
+  //         return { ...prevBox, on: !prevBox.on };
+  //       } else {
+  //         return prevBox;
+  //       }
+  //     });
+  //   });
+  // }
+
+  function toggle(id) {
+    setBox((prevBox) => {
+      return prevBox.map((box) => {
+        return box.id === id ? { ...box, on: !box.on } : box;
+      });
+    });
+  }
+
   const boxElements = box.map((square) => (
-    <Box key={square.id} on={square.on} />
+    <Box key={square.id} on={square.on} handleClick={() => toggle(square.id)} />
   ));
+
   return <div className="box--wrapper">{boxElements}</div>;
 }
-
 export default App;
--- src/Box.js
+++ src/Box.js
@@ -1,25 +1,9 @@
 import React from "react";
 
-/**
- * Challenge: Create state controlling whether
- * this box is "on" or "off". Use the incoming
- * `props.on` to determine the initial state.
- *
- * Create an event listener so when the box is clicked,
- * it toggles from "on" to "off".
- *
- * Goal: clicking each box should toggle it on and off.
- */
 export default function (props) {
-  const [boxState, isBoxOn] = React.useState(props.on);
-
-  function toggle() {
-    isBoxOn((prevBoxState) => !prevBoxState);
-  }
-
   const styles = {
-    backgroundColor: boxState ? "#222222" : "#cccccc",
+    backgroundColor: props.on ? "#222222" : "#cccccc",
   };
 
-  return <div className="box" style={styles} onClick={toggle} />;
+  return <div className="box" style={styles} onClick={props.handleClick} />;
 }
